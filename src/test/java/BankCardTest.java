b14cfc110ed10ae7263b6047891fd1ee7c44ce7f
Ondine <ondinevika@gmail.com>
2023-07-06 13:22:33 +0300

    change tests

--- src/test/java/BankCardTest.java
+++ src/test/java/BankCardTest.java
@@ -43,8 +43,8 @@ public class BankCardTest {
     void testDriver() {
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Ахматова Анна");
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+71211122233");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.className("button")).click();
+        driver.findElement(By.cssSelector("[data-test-id='agreement'] ")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
         String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
